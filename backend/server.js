efbec652356d69131f69badaedcdd9b0b3c5c05a
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-06-14 07:26:18 +0530

    final_commit

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://portfolio-frontend-kmoh.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type']
 }));
