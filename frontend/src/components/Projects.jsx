b80ff57a159eda74a38c1f916876ec5716c0b941
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-11 21:25:43 +0530

    fix

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -38,7 +38,7 @@ const projects = [
     image:'https://mobisoftinfotech.com/resources/wp-content/uploads/2018/07/Banner-1.png',
     tags: ['React', 'Node.js', 'MongoDB', 'Express.js','Tailwindcss'],
     githubLink: 'https://github.com/Dhanush18100/CareConnect.git',
-    liveLink: 'https://github.com/Dhanush18100/CareConnect.git',
+    liveLink: '',
   },
   {
      title: 'QuickGPT',
@@ -101,7 +101,7 @@ const Projects = () => {
                     Code
                   </a>
                   <a
-                    href={project.liveLink}
+                    href={project.liveLink || 'Not Deployed'}
                     target="_blank"
                     rel="noopener noreferrer"
                     className="project-link live"
