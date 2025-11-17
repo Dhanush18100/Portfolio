bc0a3ff66ef860de04cb123a581046110852953a
Dhanush18100 <dhanush.nayak.100@gmail.com>
2025-11-17 18:46:54 +0530

    fix

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -5,7 +5,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS    // must be APP PASSWORD
+    pass: process.env.EMAIL_APP_PASSWORD   // must be APP PASSWORD
   }
 });
 
